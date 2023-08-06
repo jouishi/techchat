ca01837b7cd8b6b042bf6df36bf4754a12d6b60a
jouishi <08251023jke01@gmail.com>
2023-08-06 13:16:09 +0900

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: '投稿が作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :author_name)
+  end
+
+
+
 end
