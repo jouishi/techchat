e9dc4379dae6d7dcf961dbece579816c87a3c586
jouishi <08251023jke01@gmail.com>
2023-08-06 18:07:16 +0900

    一覧画面修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+
 
   resources :posts, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
